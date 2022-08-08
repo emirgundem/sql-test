e26062d480f2d6acb289fba60841439822798eba
emircan gundem <emirgndem@gmail.com>
2022-08-08 16:24:58 +0300

    Create test3.sql

---
+++ test3.sql
@@ -0,0 +1,8 @@
+select * from country where country like 'A%a'
+
+select * from country where country Ilike '______%n'
+
+select title from film where title Ilike 'T____%' 
+
+select * from film where title like 'C%' and length > 90 and rental_rate = 2.99
+
