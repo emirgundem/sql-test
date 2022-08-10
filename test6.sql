a04245f29b00e1eac84aa18676399de82a6a4db7
emircan gundem <emirgndem@gmail.com>
2022-08-10 15:57:00 +0300

    Create test6.sql

---
+++ test6.sql
@@ -0,0 +1,12 @@
+Aggregate Fonksiyonları
+
+select avg(amount) from payment
+
+select min(amount) from payment
+
+select max(amount) from payment
+
+select sum(amount) from payment
+
+select count(*) from payment
+
