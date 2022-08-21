3034cfa2765f30dc43dae6dd5ead9815386f0009
emircan gundem <emirgndem@gmail.com>
2022-08-21 22:51:01 +0300

    Create test12.sql

---
+++ test12.sql
@@ -0,0 +1,13 @@
+--SORU-1
+select count(*) from film where length > (select avg(length) from film);
+
+--SORU -2 
+select count(*) from film where rental_rate = (select max(rental_rate) from film);
+
+--SORU -3
+
+select * from film where rental_rate = (select min(rental_rate) from film) and replacement_cost = (select min(replacement_cost) from film);
+
+--SORU -4
+
+select customer_id, sum(amount) from payment group by customer_id order by sum(amount) desc;
